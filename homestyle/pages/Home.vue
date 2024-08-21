4ce41ed608fd3c9e9e08b884a07efccbc44be18d
simys333 <156407725+simys333@users.noreply.github.com>
2024-08-21 15:51:20 +0530

    Update Home.vue

--- homestyle/pages/Home.vue
+++ homestyle/pages/Home.vue
@@ -4,6 +4,7 @@
       :button-text="hero.buttonText" :link="hero.link" :image-src="hero.imageSrc" :image-width="hero.imageWidth"
       :image-height="hero.imageHeight" :nuxt-img-config="hero.imageConfig" image-tag="nuxt-img" />
     <!-- New section with four boxes -->
+    
     <div class="icon-text-row">
       <div class="icon-text-box">
         <div class="icon">
