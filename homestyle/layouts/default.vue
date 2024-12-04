ba6421df3180755b0a7f6fa6be17ee31199d60f7
Simy <simy@onbyz.com>
2024-12-04 18:13:57 +0530

    productcard

--- homestyle/layouts/default.vue
+++ homestyle/layouts/default.vue
@@ -278,6 +278,6 @@ a.sf-link.sf-header-navigation-item__link.nav-item:nth-child(2):hover
   width:216px !important;
 }
 #product .sf-carousel .sf-product-card {
-    --product-card-max-width: 15.5rem;
+    --product-card-max-width: 15.5rem !important;
 }
 </style>
